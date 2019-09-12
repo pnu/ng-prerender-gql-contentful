05ee4199e100d13e05867975504815c28de33592
Panu Ervamaa <panu.ervamaa@frantic.com>
2019-09-12 13:32:37 +0300

    add lazy modules contact, work and home
    ng g module contact --routing true --route contact --module app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }, { path: 'work', loadChildren: () => import('./work/work.module').then(m => m.WorkModule) }, { path: 'contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
