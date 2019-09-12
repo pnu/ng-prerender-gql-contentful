062fc2e549ea45f0efed0e4da670a97f6f2d86df
Panu Ervamaa <panu.ervamaa@frantic.com>
2019-09-12 13:48:10 +0300

    yarn add preboot, configure it to the AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { PrebootModule } from 'preboot';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,6 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    PrebootModule.withConfig({ appRoot: 'app-root' }),
     AppRoutingModule
   ],
   providers: [],
