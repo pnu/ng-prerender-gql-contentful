7d875c7495fa76aee48dab3f565ff775a18c38d5
Panu Ervamaa <panu.ervamaa@frantic.com>
2019-09-12 13:34:41 +0300

    add route { path: '**', redirectTo: 'home' }

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }, { path: 'work', loadChildren: () => import('./work/work.module').then(m => m.WorkModule) }, { path: 'contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule) }];
+const routes: Routes = [
+  { path: 'work', loadChildren: () => import('./work/work.module').then(m => m.WorkModule) },
+  { path: 'contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule) },
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: '**', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
