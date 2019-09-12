535d91cf43358aa88c4a594cadd6f2916e1022ba
Panu Ervamaa <panu.ervamaa@frantic.com>
2019-09-12 16:36:48 +0300

    GraphQLModule: define endpoint uri

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -3,7 +3,7 @@ import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
 import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 
-const uri = ''; // <-- add the URL of the GraphQL server here
+const uri = 'https://graphql.contentful.com/content/v1/spaces/bswbys9qvg7q?access_token=0AC3sRBQaFKYMFE49zQrcgVYe-aXpOw4dlB8gcpiYBE'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink) {
   return {
     link: httpLink.create({uri}),
